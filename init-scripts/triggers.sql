b426f47968ad26050dcf0683285b07bd081506ce
Yehor Onufreichuk <572519065z@gmail.com>
2024-05-20 15:36:15 +0300

    SQL trigger to prevent negative payment amount impl

--- init-scripts/triggers.sql
+++ init-scripts/triggers.sql
@@ -30,4 +30,21 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER prevent_self_messaging_trigger
     BEFORE INSERT OR UPDATE ON "message"
     FOR EACH ROW
-    EXECUTE FUNCTION prevent_self_messaging();
+EXECUTE FUNCTION prevent_self_messaging();
+
+-- Payment triggers
+
+CREATE OR REPLACE FUNCTION prevent_negative_amount()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.amount < 0 THEN
+        RAISE EXCEPTION 'Amount cannot be negative';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_negative_amount_trigger
+    BEFORE INSERT OR UPDATE ON "payment"
+    FOR EACH ROW
+EXECUTE FUNCTION prevent_negative_amount();
