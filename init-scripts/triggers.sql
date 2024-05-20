391e82572b55e9c987d7261ee84b7457e61967cb
Yehor Onufreichuk <572519065z@gmail.com>
2024-05-20 15:40:25 +0300

    SQL trigger to validate end date against start date impl

--- init-scripts/triggers.sql
+++ init-scripts/triggers.sql
@@ -65,3 +65,20 @@ CREATE TRIGGER prevent_negative_bid
     BEFORE INSERT OR UPDATE ON "bid"
     FOR EACH ROW
 EXECUTE FUNCTION prevent_negative_bid();
+
+-- Auction triggers
+
+CREATE OR REPLACE FUNCTION check_dates()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.end_date < NEW.start_date THEN
+        RAISE EXCEPTION 'End date cannot be earlier than start date';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_dates
+    BEFORE INSERT OR UPDATE ON "auction"
+    FOR EACH ROW
+EXECUTE FUNCTION check_dates();
