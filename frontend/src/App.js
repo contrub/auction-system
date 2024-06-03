9be2924a92a5ca28d86bf1aeefeac41bc1465827
Yehor <572519065z@gmail.com>
2024-06-03 11:03:37 +0300

    auctions & finance stats pages impl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,11 @@ import Navbar from "./components/Navbar"
 import Login from "./pages/auth/Login"
 import SignUp from "./pages/auth/SignUp"
 
+import Auctions from "./pages/Auctions";
+import Auction from "./pages/Auction";
 import Users from "./pages/Users";
 import User from "./pages/User"
+import FinancialStats from "./pages/stats/FinancialStats";
 
 function App() {
   return (
@@ -21,6 +24,7 @@ function App() {
             <Route path="/auctions/:id" element={ <Auction/> }/>
             <Route path="/users" element={ <Users/> }/>
             <Route path="/users/:id" element={ <User/> }/>
+            <Route path="/financial-stats" element={ <FinancialStats/> }/>
         </Routes>
     </BrowserRouter>
   );
